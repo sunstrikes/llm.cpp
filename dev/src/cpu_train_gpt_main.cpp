f993b9f42a22469d9b6e689b4bd3184980155071
sunminqi <sunminqi@sunminqideMacBook-Pro.local>
2024-05-11 15:45:15 +0800

    add CMakeLists

---
+++ dev/src/cpu_train_gpt_main.cpp
@@ -0,0 +1,10 @@
+//
+// Created by Sun,Minqi on 2024/4/28.
+//
+
+#include <iostream>
+#include "param.h"
+
+int main() {
+  return 0;
+}
--- dev/src/param.h
+++ dev/src/param.h
@@ -4,5 +4,9 @@
 
 #ifndef LLM_CPP_DEV_SRC_PARAM_H_
 #define LLM_CPP_DEV_SRC_PARAM_H_
-
+namespace llm {
+template <typename T>
+  class Param {
+  };
+}
 #endif // LLM_CPP_DEV_SRC_PARAM_H_
